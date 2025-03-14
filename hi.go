594a3091e9e2e9e17cfdaf550f332815b880dbb3
dztpadm <admin@dztpp.ru>
2025-03-14 20:15:41 +0300

    Commit_002

--- hi.go
+++ hi.go
@@ -3,6 +3,6 @@ package hi
 
 // Объявление функции, которая возвращает строку:
 func Hi() string {
-    str := "<h1>Привет, мои любимые: Pascal/Assembly/C/Go/JS!</h1>"
+    str := "<h1>Привет, мой непостижимый мир!</h1>"
     return str
 }
